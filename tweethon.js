7967266529525fc585dbd23ce27ee8a72afad969
m00n <m00n@chillaz-net.de>
2010-10-31 18:50:34 +0100

    added a function to make changes in modeldata easier

--- tweethon.js
+++ tweethon.js
@@ -3,3 +3,21 @@ function each(l, f) {
         f(i, l[i]);
     }
 }
+
+function changeEntry (model, id_field, id_value, ch_field, ch_value) {
+    for (var i = 0; i < model.count; i ++) {
+        var data = model.get(i);
+        if (data[id_field] == id_value) {
+            data[ch_field] = ch_value
+        }
+        model.set(i, data);
+    }
+}
+
+function indexFor (model, id_field, id_value) {
+    for (var i = 0; i < account_model.count; i ++) {
+        var data = account_model.get(i);
+        if (data[id_field] == id_value)
+            return i
+    }
+}
