b9dd2da5b7536d79a649667fec6985438a83962d
m00n <m00n@chillaz-net.de>
2010-11-03 00:17:14 +0100

    tweet storing and restoring works

--- tweethon.js
+++ tweethon.js
@@ -23,45 +23,35 @@ function indexFor (model, id_field, id_value) {
 }
 
 function restoreTweets (account, type, args, to_model) {
-    var index_found = -1;
-    each(tweet_store, function (index, panel) {
-        if (panel.account == account && panel.type == type && panel.args == args) {
-            each(tweet_store.tweets, function (index, tweet) {
-                to_model.append(tweet);
-            });
-            index_found = index;
+    var tweets = tweet_store.load(
+        {
+            "account": account,
+            "type": type,
+            "args": args,
         }
-    });
-
-    if (index_found > -1) {
-        tweet_store.splice(index_found, 1);
+    );
+    if (tweets)
+    {
+        each(tweets, function (index, tweet) { to_model.append(tweet); });
     }
-    return index_found
 }
 
 function storeTweets (account, type, args, from_model) {
-    var tweets = [];
-    for (var i = 0; i < from_model.count; i++) {
-        tweets.push(from_model.get(index));
-    }
-
     var panel_data = {
         "account": account,
         "type": type,
         "args": args,
-        "tweets": tweets
+        "tweets": [],
     };
-    tweet_store.push(panel_data);
+
+    for (var i = 0; i < from_model.count; i++) {
+        var model_object = from_model.get(i);
+        var js_object = new Object();
+        for (var attr in model_object) {
+            js_object[attr] = model_object[attr];
+        }
+        panel_data.tweets.push(js_object);
+    }
+    tweet_store.store(panel_data);
 
 }
-
-var tweet_store = [
-    /*
-    {
-        "account": 0,
-        "type": 0,
-        "args": 0,
-        "tweets": 0,
-    }
-    */
-]
