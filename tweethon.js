efe1ed37829db6f683c3b772cb6ff2650a814944
Yannick Schäfer <m00n@chillaz-net.de>
2010-11-02 22:28:56 +0100

    added a tweetstore to persist tweets for delegates which are destroyed because they aren't visible

--- tweethon.js
+++ tweethon.js
@@ -21,3 +21,47 @@ function indexFor (model, id_field, id_value) {
             return i
     }
 }
+
+function restoreTweets (account, type, args, to_model) {
+    var index_found = -1;
+    each(tweet_store, function (index, panel) {
+        if (panel.account == account && panel.type == type && panel.args == args) {
+            each(tweet_store.tweets, function (index, tweet) {
+                to_model.append(tweet);
+            });
+            index_found = index;
+        }
+    });
+
+    if (index_found > -1) {
+        tweet_store.splice(index_found, 1);
+    }
+    return index_found
+}
+
+function storeTweets (account, type, args, from_model) {
+    var tweets = [];
+    for (var i = 0; i < from_model.count; i++) {
+        tweets.push(from_model.get(index));
+    }
+
+    var panel_data = {
+        "account": account,
+        "type": type,
+        "args": args,
+        "tweets": tweets
+    };
+    tweet_store.push(panel_data);
+
+}
+
+var tweet_store = [
+    /*
+    {
+        "account": 0,
+        "type": 0,
+        "args": 0,
+        "tweets": 0,
+    }
+    */
+]
